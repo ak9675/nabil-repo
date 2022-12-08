55c5429bf7b011c5f4e56076f7ade6a20ca11808
ak9675 <98090519+ak9675@users.noreply.github.com>
2022-12-08 20:06:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "example" {
   location = "eastus"
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example1" {
   name     = "tfrg1"
   location = "westus"
 }
