60a8ff83ccd8d16ef0b7ce45f38cb8660ad206d0
ak9675 <98090519+ak9675@users.noreply.github.com>
2022-12-08 20:02:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = var.rg_name
+  name     = "tfrg"
   location = "eastus"
 }
 
 resource "azurerm_public_ip" "example" {
-  name                = "acceptanceTestPublicIp1"
+  name                = "tfpip"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
