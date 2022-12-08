9f48ec96de69e6bfd35802570825693ac02dabbe
ak9675 <98090519+ak9675@users.noreply.github.com>
2022-12-08 20:05:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ resource "azurerm_resource_group" "example" {
   location = "eastus"
 }
 
+resource "azurerm_resource_group" "example" {
+  name     = "tfrg1"
+  location = "westus"
+}
+
 resource "azurerm_public_ip" "example" {
   name                = "tfpip"
   resource_group_name = azurerm_resource_group.example.name
